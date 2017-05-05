1415d73d7e0b261301f4e813b0d1e575d38d17d0
Atul Bhosale <atul1bhosale@gmail.com>
2017-05-05 12:02:34 +0530

    Get numbers in output instead of the entire object

---
+++ numbers_2.js
@@ -0,0 +1,19 @@
+function* createCounter() {
+  yield 1
+  yield 2
+  yield 3
+  yield 4
+}
+
+const counter = createCounter();
+
+for(let i of counter) {
+  console.log(i);
+}
+
+/* Output -
+1
+2
+3
+4
+*/
